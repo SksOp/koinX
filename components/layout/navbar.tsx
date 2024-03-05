76ff3e4a3cd56b4b1f6b07a63a3b7744db485608
SksOp <shubhaman47@gmail.com>
2024-03-06 04:11:51 +0530

    fix: Remove unused imports from navbar component

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,13 +1,9 @@
 "use client";
 import {
   NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
 } from "../ui/navigation-menu";
 import {
   DropdownMenu,
