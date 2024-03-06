6f2212a7eb930d369f1e8c7d0a605462d9cc30ae
SksOp <shubhaman47@gmail.com>
2024-03-06 13:03:38 +0530

    Refactor CoinGeko class and move getSimplePrice function to actions/coin-geko.tsx

--- actions/coin-geko.tsx
+++ actions/coin-geko.tsx
@@ -5,3 +5,12 @@ export async function searchTranding() {
   if (!res.ok) throw new Error("Unable to fetch the coin details");
   return await res.json();
 }
+
+export async function getSimplePrice(ids: string) {
+  const vs_currencies = "inr,usd";
+  const res = await fetch(
+    `${baseUrl}/simple/price?ids=${ids}&vs_currencies=${vs_currencies}`
+  );
+  if (!res.ok) throw new Error("Unable to fetch the coin details");
+  return await res.json();
+}
