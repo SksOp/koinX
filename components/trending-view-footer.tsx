fc175241f5d6b5cb7f257f35e191f639cd5c38fe
SksOp <shubhaman47@gmail.com>
2024-03-07 03:01:31 +0530

    feat: Add link to coin details in trending view footer

--- components/trending-view-footer.tsx
+++ components/trending-view-footer.tsx
@@ -11,6 +11,7 @@ import { Card, CardContent, CardHeader } from "./ui/card";
 import Image from "next/image";
 import PercentViewer from "./percent-viewer";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   trendings: Trendings;
@@ -59,34 +60,36 @@ interface CoinProps {
 const CoinConponent: React.FC<CoinProps> = ({ coin }) => {
   return (
     <CarouselItem className="max-w-[18rem]">
-      <Card>
-        <CardHeader className="flex items-center flex-row gap-1 w-full">
-          <Image
-            width={200}
-            height={200}
-            src={coin.item.large}
-            alt={coin.item.name}
-            className="w-8 rounded-full"
-          />
-          <p>{coin.item.name}</p>
-          <PercentViewer
-            variant="mini"
-            percent={coin.item.data.price_change_percentage_24h.usd}
-          />
-        </CardHeader>
-        <CardContent>
-          <p className="text-lg font-semibold">
-            {coin.item.data.price.split("<")[0]}
-          </p>
-          <Image
-            width={200}
-            height={200}
-            className="w-auto h-12 m-auto"
-            src={coin.item.data.sparkline}
-            alt={coin.item.name}
-          />
-        </CardContent>
-      </Card>
+      <Link href={`/${coin.item.id}`}>
+        <Card>
+          <CardHeader className="flex items-center flex-row gap-1 w-full">
+            <Image
+              width={200}
+              height={200}
+              src={coin.item.large}
+              alt={coin.item.name}
+              className="w-8 rounded-full"
+            />
+            <p>{coin.item.name}</p>
+            <PercentViewer
+              variant="mini"
+              percent={coin.item.data.price_change_percentage_24h.usd}
+            />
+          </CardHeader>
+          <CardContent>
+            <p className="text-lg font-semibold">
+              {coin.item.data.price.split("<")[0]}
+            </p>
+            <Image
+              width={200}
+              height={200}
+              className="w-auto h-12 m-auto"
+              src={coin.item.data.sparkline}
+              alt={coin.item.name}
+            />
+          </CardContent>
+        </Card>
+      </Link>
     </CarouselItem>
   );
 };
